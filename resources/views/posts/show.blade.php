8543d6b90cd53bbe6f631d57c5e8ecaaf1833d74
Richard Hall <rd.hall91@gmail.com>
2017-08-12 14:48:04 -0400

    Laravel 5.4 15

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -7,6 +7,23 @@
         <p class="blog-post-meta">{{ $post->created_at->toFormattedDateString() }}</p>
 
         {{ $post->body }}
+
+        <hr>
+
+        <div class="comments">
+            <ul class="list-group">
+                @foreach($post->comments as $comment)
+                    <li class="list-group-item">
+                        <strong>
+                            {{ $comment->created_at->diffForHumans() }} : &nbsp;
+                        </strong>
+
+
+                        {{ $comment->body }}
+                    </li>
+                @endforeach
+            </ul>
+        </div>
     </div>
 
 @endsection
