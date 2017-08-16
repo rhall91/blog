4c5216d7e70c6dfa28a8c097b832775b21837a2d
rhall91 <richard@furrylogic.net>
2017-08-16 13:40:51 -0400

    Laravel 5.4 19

--- app/User.php
+++ app/User.php
@@ -32,4 +32,9 @@ class User extends Authenticatable
         return $this->hasMany(Post::class);
     }
 
+    public function publish(Post $post)
+    {
+        $this->posts()->save($post);
+    }
+
 }
